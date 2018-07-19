3125821deba2c0b7ce86141bcab49432e5e25b65
git <zq13408401721@139.com>
2018-07-19 17:36:35 +0800

    初始化

--- app/src/main/java/com/sdcard/MainActivity.kt
+++ app/src/main/java/com/sdcard/MainActivity.kt
@@ -2,11 +2,31 @@ package com.sdcard
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setOnClickListener()
+    }
+
+    fun setOnClickListener(){
+        btn_readcard.setOnClickListener(this)
+        btn_writecard.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        when(v!!.id){
+            R.id.btn_readcard->{
+
+            }
+            R.id.btn_writecard->{
+
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/sdcard/SdCardUtils.java
@@ -0,0 +1,11 @@
+package com.sdcard;
+
+/**
+ * Created by yun on 18/7/19.
+ */
+
+public class SdCardUtils {
+
+
+
+}
